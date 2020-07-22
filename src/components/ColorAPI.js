000e10396d1f28531993da2d575fce41ddf163fc
holdenmad <holden.madagame@gmail.com>
2020-07-22 15:07:18 +0200

    fetch api

---
+++ src/components/ColorAPI.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// const url = "http://www.thecolorapi.com/id?hex=24B1E0";
+
+
+
+const ColorAPI = props => {
+    return (
+        <div>
+            <div>The color is </div>
+        </div>
+    )
+}
+
+// //Fetch API
+// fetch(url)
+//   .then(response => response.json())
+//   .then(color => console.log(color));
+
+export default ColorAPI
