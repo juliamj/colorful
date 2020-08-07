f571a09b26028db62dd1a9e23756dace785be226
Julia Jannsen <jannsen.julia@gmx.de>
2020-08-07 14:43:32 +0200

    added Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { useState, useEffect } from "react";
 // import axios from "axios";
 import "./styles/App.css";
+import "./styles/Header.css";
+import Header from './components/Header';
+import About from './components/About';
+import Home from './components/Home';
+import ColorInfo from "./components/ColorInfo";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-
-	return (
-		<div className="App">
-			<h1>Color App</h1>
-        </div>
+  return (
+	
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path={"/"} exact component={Home} />
+          <Route path="/about" component={About}  />
+          <Route path={"/:id"} component={ColorInfo} />
+        </Switch>
+      </div>
+    </Router >
   );
 }
 
+
+
 export default App;
+
+
