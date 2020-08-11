e55f629dbcd899c8b3877416dbfd3f3e7e740208
Julia Jannsen <jannsen.julia@gmx.de>
2020-08-11 16:20:06 +0200

    connect json-data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 // import axios from "axios";
 import { Box, Text } from "grommet";
 import "./styles/App.css";
@@ -8,18 +8,37 @@ import About from "./components/About";
 import Footer1 from "./components/Footer";
 import ColorInfo from "./components/ColorInfo";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from 'axios'
+import colors from './colors.json'
 
 // import MainSwatches from './components/MainSwatches'
 // import FooterNav from './components/FooterNav'
 
 function App() {
+  const [color, setColor] = useState([]);
+  // useEffect(() => {
+	// 	const fetchColor = async () => {
+	// 		const color = await axios
+  //       .get('https://colors.herokuapp.com/colors')
+  //       .then(result => result.data)
+  //     return color;
+      
+	// 	};
+	// 	fetchColor().then(res => setColor(res));
+  // }, []);
   return (
     <Box alignContent="center" className="App">
       <Header />
       <Switch>
-        <Route path={"/"} exact component={Home} />
+        {/* <Route path={"/"} exact component={Home} /> */}
+        <Route exact path={"/"}>
+          <Home colors={colors}/>
+        </Route>
         <Route path="/about" component={About} />
-        <Route path={"/:hexcode"} component={ColorInfo} />
+        <Route path={"/:cn"}>
+          <ColorInfo colors={colors}/>
+        </Route>
+        {/* <Route path={"/:cn"} component={ColorInfo} /> */}
       </Switch>
 
       <Footer1 />
@@ -28,3 +47,21 @@ function App() {
 }
 
 export default App;
+
+// const renderColors = data => {
+// 	const colors = data.results;
+//   console.debug({ colors });
+//   return colors.map(color => {
+// 		return (ul.innerHTML += `
+//     <li>
+//       <img src="${color.picture.medium}" />
+//       <p>${color.name.first} ${user.name.last}</p>
+//       <div>${color.email}</div>
+//     </li>`);
+// 	});
+// };
+
+// const url = "https://randomuser.me/api/?results=10";
+// fetch(url)
+// 	.then(response => response.json())
+// 	.then(renderColors);
--- src/components/ColorInfo.js
+++ src/components/ColorInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
 import {
     Carousel,
     Image,
@@ -7,43 +8,57 @@ import {
     Grid,
 } from "grommet";
 
-function ColorInfo() {
+
+function ColorInfo({colors}) {
+    
+    // let {cn} = useParams()
+
+    // cn = colors.colorName.split(' ').join('-')  
+    // cn = cn.split('’').join('-')  
+
     return (
+        <Box aligSelf="center" margin="medium" background="orange">
+            {Object.entries(colors).map((color, index) => {
+                return (
 
-        <Box aligSelf="center"  margin="medium" background="orange">
-            <Grid margin="medium"
-                aligSelf="center" justifyContent="center"
-                rows={['xsmall', 'xsmall', 'xsmall', 'xsmall', 'xsmall']}
-                
-                columns={['small', 'small', 'medium']}
-                gap="small"
-                areas={[
-                    { name: 'heading', start: [0, 0], end: [2, 0] },
-                    { name: 'description', start: [0, 1], end: [2, 1] },
-                    { name: 'info', start: [0, 2], end: [0, 2] },
-                    { name: 'languages', start: [1, 2], end: [1, 2] },
-                    { name: 'images', start: [2, 2], end: [2, 3] },
-                    { name: 'history', start: [0, 3], end: [1, 3] },
-                    { name: 'quotes', start: [0, 4], end: [2, 4] },
-                ]}
+                <Grid margin="medium"
+                    aligSelf="center" justifyContent="center"
+                    rows={['xsmall', 'xsmall', 'xsmall', 'xsmall', 'xsmall']}
 
-            >
-                <Box pad="medium" gridArea="heading" size="large" align="center"><Heading pad="medium" > Colorname</Heading></Box>
-                <Box pad="small" gridArea="description" background="light-5" align="center">Description</Box>
-                <Box pad="small" gridArea="info" background="light-5" >Info: Hex/RGB</Box>
-                <Box pad="small" gridArea="languages" background="light-5" >Languages</Box>
-                <Box pad="small" gridArea="history" background="light-5" >History</Box>
-                <Box pad="small" gridArea="images" background="light-5" ><Box height="small" width="medium" overflow="hidden">
-                    <Carousel fill>
-                        <Image fit="cover" src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg" />
-                        <Image fit="cover" src="//v2.grommet.io/assets/IMG_4245.jpg" />
-                        <Image fit="cover" src="//v2.grommet.io/assets/IMG_4210.jpg" />
-                    </Carousel>
-                </Box></Box>
-                <Box pad="small" gridArea="quotes" background="light-2" align="center">Quotes</Box>
-            </Grid>
+                    columns={['small', 'small', 'medium']}
+                    gap="small"
+                    areas={[
+                        { name: 'heading', start: [0, 0], end: [2, 0] },
+                        { name: 'description', start: [0, 1], end: [2, 1] },
+                        { name: 'info', start: [0, 2], end: [0, 2] },
+                        { name: 'languages', start: [1, 2], end: [1, 2] },
+                        { name: 'images', start: [2, 2], end: [2, 3] },
+                        { name: 'history', start: [0, 3], end: [1, 3] },
+                        { name: 'quotes', start: [0, 4], end: [2, 4] },
+                    ]}
+                >
+                    <Box pad="medium" gridArea="heading" size="large" align="center"><Heading pad="medium" >Colorname: {color.colorName}</Heading></Box>
+                    {/* <Box pad="small" gridArea="description" background="light-5" align="center">
+                        <h2>Description</h2><br></br>{color.description}</Box>
+                    <Box pad="small" gridArea="info" background="light-5" >Info: Hex/RGB{color.hex}{colors.rgb}</Box>
+                    <Box pad="small" gridArea="languages" background="light-5" >
+                        <ul>Languages
+                            <li>German: {color.languages.german}</li>
+                            <li>Spanish: {color.languages.spanish}</li>
+                            </ul></Box>
+                    <Box pad="small" gridArea="history" background="light-5" >History{color.history}</Box>
+                    <Box pad="small" gridArea="images" background="light-5" ><Box height="small" width="medium" overflow="hidden">
+                        <Carousel fill>
+                            <Image fit="cover" src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg" />
+                            <Image fit="cover" src="//v2.grommet.io/assets/IMG_4245.jpg"/>
+                            <Image fit="cover" src="//v2.grommet.io/assets/IMG_4210.jpg"/>
+                        </Carousel>
+                    </Box></Box>
+                    <Box pad="small" gridArea="quotes" background="light-2" align="center" ><h2>Quote</h2>"<p className="italic">{color.quote}"</p></Box> */}
+                </Grid>)
+                {console.log(color)}
+            })}
         </Box>
-
     );
 }
 
